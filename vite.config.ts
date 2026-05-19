72cb68fe424563d5ac89737f8434abf5abd4b649
danielMosvich <daniel.mosvich@gmail.com>
2026-05-18 22:14:44 -0500

    agregacion de netlify

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
+import netlify from "@netlify/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    netlify(),
     babel({ presets: [reactCompilerPreset()] }),
   ],
 });
